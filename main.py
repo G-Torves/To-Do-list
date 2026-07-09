449f5c6a73c933d59f833542701cf98e04617e9b
Kazinho0 <otakinhogtg@gmail.com>
2026-07-09 15:39:17 -0300

    0.3.7 - Criptografia inicial

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from passlib.context import CryptContext
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # Rodar com: uvicorn main:app --reload
 app = FastAPI()
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated='auto')
+
 from task_routes import task_router
 from auth_routes import auth_router
 
